9c96ba91b4738fbd3423bf1bd537a2406762d921
Saba Pathan <shaikh.saba88@gmail.com>
2022-09-28 22:11:10 +0530

    add start script

--- app.js
+++ app.js
@@ -159,6 +159,7 @@ app.use((err,req,res,next)=>{
     res.render('error',{err});
 });
 
-app.listen(3000,()=>{
-    console.log("PORT 3000");
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`PORT ${port}`);
 });
